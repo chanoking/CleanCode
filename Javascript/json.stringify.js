414293bfe072e8e06b12f14ceec5a1a1e52a74ad
진찬호 <chanhojin94@kakao.com>
2024-01-08 20:11:05 +0900

    stringyfy.json

--- Javascript/json.stringify.js
+++ Javascript/json.stringify.js
@@ -9,3 +9,21 @@ console.log(JSON.stringify({ a: [10, undefined, function () {}, Symbol("")] }));
 console.log(JSON.stringify(new Date(2006, 0, 2, 15, 4, 5)));
 
 console.log(new Date(2006, 0, 2, 15, 4, 5));
+
+function replacer(key, value) {
+  if (typeof value === "string") {
+    return undefined;
+  }
+  return value;
+}
+
+const foo = {
+  foundation: "Mozilla",
+  model: "box",
+  week: 45,
+  transport: "car",
+  month: 7,
+};
+
+const hi = JSON.stringify(foo, replacer);
+console.log(hi);
