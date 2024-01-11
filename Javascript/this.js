95b443269c05688feb170747d8de89ede6ca2b6e
진찬호 <chanhojin94@kakao.com>
2024-01-11 12:55:48 +0900

    this

--- Javascript/this.js
+++ Javascript/this.js
@@ -1,3 +1,4 @@
+// "use strict";
 const test = {
   prop: 42,
   func: function () {
@@ -43,19 +44,255 @@ const globalObject = this;
 const foo = () => this;
 console.log(foo() === globalObject);
 
-function C() {
-  this.a = 28;
+// function C() {
+//   this.a = 28;
+// }
+
+// let o = new C();
+
+// console.log(o.a);
+
+// function C2() {
+//   this.a = 280;
+//   return { a: 290 };
+// }
+
+// const o2 = new C2();
+
+// console.log(o2.a);
+
+// class C {
+//   instanceField = this;
+//   static staticField = this;
+// }
+
+// const c = new C();
+// console.log(c.instanceField === c);
+// console.log(C.staticField === C);
+
+// class C {
+//   instanceField = this;
+//   static staticField = this;
+// }
+
+// const c = new C();
+// console.log(c.instanceField === c);
+// console.log(C.staticField === C);
+
+class C {
+  instanceField = this;
+  static staticField = this;
 }
 
-let o = new C();
+const c = new C();
 
-console.log(o.a);
+console.log(c.instanceField === c);
+console.log(C.staticField === C);
 
-function C2() {
-  this.a = 280;
-  return { a: 290 };
+// console.log(this === window);
+
+const obj = {
+  a: this,
+};
+
+// console.log(obj.a === window);
+
+// function add(c, d) {
+//   return this.a + this.b + c + d;
+// }
+
+// const o = { a: 1, b: 3 };
+
+// console.log(add.call(o, 5, 7));
+
+// console.log(add.apply(o, [10, 20]));
+
+function add(c, d) {
+  return this.a + this.b + c + d;
 }
 
-const o2 = new C2();
+const o = { a: 1, b: 3 };
 
-console.log(o2.a);
+console.log(add.call(o, 5, 7));
+console.log(add.apply(o, [10, 20]));
+
+function bar() {
+  console.log(Object.prototype.toString.call(this));
+}
+
+bar.call(7);
+bar.call("foo");
+bar.call(undefined);
+
+const obj9 = {
+  getThisGetter() {
+    const getter = () => this;
+    return getter;
+  },
+};
+
+const fn = obj9.getThisGetter();
+console.log(fn() === obj9);
+
+// function sum() {
+//   return this.a + this.b + this.c;
+// }
+
+// const o2 = {
+//   a: 1,
+//   b: 2,
+//   c: 3,
+//   get average() {
+//     return (this.a + this.b + this.c) / 3;
+//   },
+// };
+
+// Object.defineProperty(o2, "sum", {
+//   get: sum,
+//   enumerable: true,
+//   configurable: true,
+// });
+
+// console.log(o2.average, o2.sum);
+
+// function sum() {
+//   return this.a + this.b + this.c;
+// }
+
+// const o2 = {
+//   a: 1,
+//   b: 2,
+//   c: 3,
+//   get average() {
+//     return (this.a + this.b + this.c) / 3;
+//   },
+// };
+
+// Object.defineProperty(o2, "sum", {
+//   get: sum,
+//   enumerable: true,
+//   configurable: true,
+// });
+
+// console.log(o2.average, o2.sum);
+
+function sum() {
+  return this.a + this.b + this.c;
+}
+
+const o2 = {
+  a: 1,
+  b: 2,
+  c: 3,
+  get average() {
+    return (this.a + this.b + this.c) / 3;
+  },
+};
+
+Object.defineProperty(o2, "sum", {
+  get: sum,
+  enumerable: true,
+  configurable: true,
+});
+
+console.log(o2.average, o2.sum);
+
+// class Car {
+//   constructor() {
+//     this.sayBye = this.sayBye.bind(this);
+//   }
+
+//   sayHi() {
+//     console.log(`Hello from ${this.name}`);
+//   }
+
+//   sayBye() {
+//     console.log(`Bye from ${this.name}`);
+//   }
+
+//   get name() {
+//     return "Ferrari";
+//   }
+// }
+
+// class Bird {
+//   get name() {
+//     return "Tweety";
+//   }
+// }
+
+// const car = new Car();
+// const bird = new Bird();
+
+// car.sayHi();
+// bird.sayHi = car.sayHi;
+// bird.sayHi();
+
+// bird.sayBye = car.sayBye;
+// bird.sayBye();
+
+// class Car {
+//   constructor() {
+//     this.sayBye = this.sayBye.bind(this);
+//   }
+//   sayHi() {
+//     console.log(`Hello from ${this.name}`);
+//   }
+
+//   sayBye() {
+//     console.log(`Bye from ${this.name}`);
+//   }
+//   get name() {
+//     return "Ferrari";
+//   }
+// }
+
+// class Bird {
+//   get name() {
+//     return "Tweety";
+//   }
+// }
+
+// const car = new Car();
+// const bird = new Bird();
+
+// car.sayHi();
+// bird.sayHi = car.sayHi;
+// bird.sayHi();
+
+// bird.sayBye = car.sayBye;
+// bird.sayBye();
+
+class Car {
+  constructor() {
+    this.sayBye = this.sayBye.bind(this);
+  }
+
+  sayHi() {
+    console.log(`Hello from ${this.name}`);
+  }
+
+  sayBye() {
+    console.log(`Bye from ${this.name}`);
+  }
+
+  get name() {
+    return "Ferrari";
+  }
+}
+
+class Bird {
+  get name() {
+    return "Tweety";
+  }
+}
+
+const car = new Car();
+const bird = new Bird();
+
+car.sayHi();
+bird.sayHi = car.sayHi;
+bird.sayHi();
+
+bird.sayBye = car.sayBye;
+bird.sayBye();
